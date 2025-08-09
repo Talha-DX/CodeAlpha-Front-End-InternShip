c5b923f56cdf485036814c89b45dd395b88479f3
user.name like "Talha <talha119@female.com>
2025-08-09 10:55:18 +0500

    InternShip

---
+++ Image Gallery/script.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".container");
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+let x=0;
+let timer;
+prev.addEventListener("click",()=>{
+x = x + 45;
+clearTimeout(timer);
+updateGallery();
+});
+next.addEventListener("click", ()=>{
+x = x - 45;
+clearTimeout(timer);
+updateGallery();
+
+});
+
+function updateGallery() {
+    container.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(()=>{
+        x = x-45;
+        updateGallery();
+    },3000);
+}
+updateGallery();
